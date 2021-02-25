df5b5edc0df631a39b063393994fc678ae9bbbe7
Виктория <31704071+victorializ@users.noreply.github.com>
2021-02-25 21:49:53 +0200

    Add files via upload

---
+++ createOutput.js
@@ -0,0 +1,13 @@
+function createOutput(data) {
+    let solution = '';
+    const separator = '\n';
+    const I = data.length;
+    solution += `${I}${separator}`;
+    data.forEach(value => {
+        solution += `${value.id}${separator}${value.lights.length}${separator}`;
+        value.lights.forEach(tl => {
+            solution += `${tl.street} ${tl.T}${separator}`;
+        });
+    });
+    return simple_solution.trim();
+}
